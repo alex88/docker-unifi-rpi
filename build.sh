9abad5f091d9779634537e42c0818f2dd4bd3726
Alessandro Tagliapietra <tagliapietra.alessandro@gmail.com>
2019-08-11 13:29:09 -0700

    Adding 5.11.38

--- build.sh
+++ build.sh
@@ -20,12 +20,12 @@ mo < templates/entrypoint.sh > "${family}/entrypoint.sh"
 chmod +x "${family}/entrypoint.sh"
 
 echo 'Building Images'
-docker build --pull -t ryansch/unifi-rpi:${docker_version}-amd64 -f ${family}/Dockerfile.amd64 ${family}
-docker build --pull -t ryansch/unifi-rpi:${docker_version}-arm32v7 -f ${family}/Dockerfile.arm32v7 ${family}
-docker build --pull -t ryansch/unifi-rpi:${docker_version}-arm64v8 -f ${family}/Dockerfile.arm64v8 ${family}
+docker build --pull -t alex88/unifi-rpi:${docker_version}-amd64 -f ${family}/Dockerfile.amd64 ${family}
+docker build --pull -t alex88/unifi-rpi:${docker_version}-arm32v7 -f ${family}/Dockerfile.arm32v7 ${family}
+docker build --pull -t alex88/unifi-rpi:${docker_version}-arm64v8 -f ${family}/Dockerfile.arm64v8 ${family}
 
 # echo 'Testing Images'
-# dgoss run ryansch/unifi-rpi:${docker_version}-amd64
-# GOSS_PATH='/usr/local/bin/goss-linux-arm' GOSS_WAIT_OPTS='-r 100s -s 1s > /dev/null' dgoss run ryansch/unifi-rpi:${docker_version}-arm32v7
+# dgoss run alex88/unifi-rpi:${docker_version}-amd64
+# GOSS_PATH='/usr/local/bin/goss-linux-arm' GOSS_WAIT_OPTS='-r 100s -s 1s > /dev/null' dgoss run alex88/unifi-rpi:${docker_version}-arm32v7
 # Testing arm64v8 is blocked on cross compiling goss for arm64
-# GOSS_PATH='/usr/local/bin/goss-linux-arm' GOSS_WAIT_OPTS='-r 70s -s 1s > /dev/null' dgoss run ryansch/unifi-rpi:${docker_version}-arm64v8
+# GOSS_PATH='/usr/local/bin/goss-linux-arm' GOSS_WAIT_OPTS='-r 70s -s 1s > /dev/null' dgoss run alex88/unifi-rpi:${docker_version}-arm64v8
